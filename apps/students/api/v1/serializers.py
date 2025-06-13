4c33d3c82f93c4f02914fe0c9d6c7192e933a428
priya-devnation <priya.francis@devnation.be>
2025-06-13 12:05:16 +0530

    files added

--- apps/students/api/v1/serializers.py
+++ apps/students/api/v1/serializers.py
@@ -7,4 +7,4 @@ from apps.students.models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['name', 'email', 'course', 'marks']
+        fields = ['id','name', 'email', 'course', 'marks']
