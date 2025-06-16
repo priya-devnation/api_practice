2877dbd41814e127658d5bc9ad02e955c64d8c28
priya-devnation <priya.francis@devnation.be>
2025-06-16 12:28:00 +0530

    files added

--- apps/booking/apps.py
+++ apps/booking/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class BookingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.booking'
-
-class EventConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.event'
