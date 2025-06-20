40c96a2a6c5e2d981d00a52a21b268621564f2d9
priya-devnation <priya.francis@devnation.be>
2025-06-20 11:40:17 +0530

    files added

--- apps/booking/models.py
+++ apps/booking/models.py
@@ -1,17 +1,17 @@
 #from django modules
 from django.db import models
-from django.core.validators import RegexValidator
 
 
 # Project modules
 from apps.event.models import Event
+from utils.validators import phone_validator
 #from apps.event.models import EventMaster
 
 # Create your models here.
 class Booking(models.Model):
     name = models.CharField(max_length=100,null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
-    ph_no = models.CharField(max_length=15, validators=[RegexValidator(regex=r'^\+?\d{10,15}$',message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")])
+    ph_no = models.CharField(max_length=15, validators=[phone_validator])
     event = models.ForeignKey(Event, on_delete = models.PROTECT, null=False, blank=False, related_name='booking_master_event_foriegn_key')  #foriegn key
     booked_at = models.DateTimeField(auto_now_add=True)
 
