851fa82b95b713144f7ef313f1b10965dc2544bd
priya-devnation <priya.francis@devnation.be>
2025-06-25 11:20:09 +0530

    uuid added

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,7 +1,12 @@
+# Django modules
 from django.db import models
+
+# Python modules
+import uuid
 
 # Create your models here.
 class Todo(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, db_index=True,unique=True)
     title = models.CharField(max_length=100,null=False,blank=False)
     description = models.CharField(max_length=100,null=False,blank=False)
     is_completed = models.BooleanField(default=False)
