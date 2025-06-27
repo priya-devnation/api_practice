0d77a84e483bff2ee4c277b65a4ad87662812bc6
priya-devnation <priya.francis@devnation.be>
2025-06-27 13:29:12 +0530

    files added

--- apps/task/api/v1/serializers.py
+++ apps/task/api/v1/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 # Project modules
 from apps.task.models import Task
 
-class TaskCreateSerializer(serializers.ModelSerializer):
+class TaskCreateUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id','uuid','task_name', 'task_description', 'task_status', 'due_date','employee_name','tags','priority']
